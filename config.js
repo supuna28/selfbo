931b43c0e2aab7fb19769c08ccc92d55048b0d4e
supuna <92195424+supuna28@users.noreply.github.com>
2022-05-08 22:19:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 
-global.owner = ['94753943957','94742134288'] // Put your number here
+global.owner = ['94753943957','94762028677'] // Put your number here
 global.mods = ['6281228402912'] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
